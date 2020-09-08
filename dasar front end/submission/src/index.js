67537c320be37e05c63518d391896eb2cf3f2836
Achmad Fadhil <afadhilprtm@gmail.com>
2020-09-08 22:29:44 +0800

    materialize + custom element

--- dasar front end/submission/src/index.js
+++ dasar front end/submission/src/index.js
@@ -1,4 +1,20 @@
+import "materialize-css/dist/css/materialize.min.css";
 import "./style/style.css";
-import $ from "jquery";
+import "materialize-css/dist/js/materialize.min";
+import main from "./scripts/main";
 
-$('#content').html('<p>Halo world!</p>');
+main();
+
+class imgCovid extends HTMLElement {
+    connectedCallback() {
+        this.src = this.getAttribute("src") || null;
+        this.alt = this.getAttribute("alt") || null;
+    
+        this.innerHTML = `
+            <img src="${this.src}"
+                alt="${this.alt}" id="imgCovid" width="100px" class="responsive-img">
+        `;
+    }
+}
+
+customElements.define("img-covid", imgCovid);
